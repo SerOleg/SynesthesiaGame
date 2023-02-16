83f10fda8c6ad460fd002cbd0d11de7d48613a43
SerOleg <58004206+SerOleg@users.noreply.github.com>
2023-02-16 14:39:24 +0000

    Initial commit

---
+++ Source/SynesthesiaGame/SynesthesiaGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SynesthesiaGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SynesthesiaGame, "SynesthesiaGame" );
+ 
